6b4f49523f509c32d20b745fc50137c74153c852
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2023-01-25 22:43:01 -0800

    wip: better recursive implementation with subtest support

--- README.md
+++ README.md
@@ -4,8 +4,20 @@ User-land NodeJS and TAP compatible test harness. Implements Node's testing API.
 
 Features:
 
-- [TAP](https://testanything.org/) formatted reports
+- [TAP](https://testanything.org/) formatted output
 - Works in [QuickJS](https://bellard.org/quickjs/)
 - Zero dependencies
 
-Quick-Tap is designed as a drop in replacement for Node's built-in test harness.
+Quick-Tap defers from Node's testing module in the following ways:
+
+- `test.skip()` is added which is currently missing in Node 19.5.0 as of writing
+- `test.todo()` is added which is currently missing in Node 19.5.0 as of writing
+- The whole harness runs in the same process and does not do any process forking
+- There's no test runner, you simply run the test file directly by executing it
+- Mocking and Assertions are not included. You can use Sinon and Chai for that
+- If the test function receives a callback function and also returns a Promise,
+  the test __will not fail__. Except, it will fail iff the callback receives any
+  truthy value as its first argument __or__ if the Promise rejects.
+
+By default the library writes to `stdout` with `console.log` calls, but you can
+override this behavior by passing a custom `reporter` function where applicable.
--- index.ts
+++ index.ts
@@ -1,92 +1,178 @@
-export class Harness {
-	private readonly _suiteStack: string[] = [];
-	private readonly _tests = new Set<TestContext.Callable>();
-	public describe(name: string, fn: TestContext.Callable) {
-		this._suiteStack.push(name);
-		fn();
-		this._suiteStack.pop();
-	}
-	public it(name: string, fn: TestContext.Callable) {
-		const testId = this._tests.size + 1;
-		this._tests.add(async () => {
-			let ok = true;
-			try {
-				await fn();
-			} catch (_) {
-				ok = false;
-			}
-			console.log(`${ok ? "ok" : "not ok"} ${testId} ${this._suiteStack.join("/")}/${name}`);
-		});
-	}
-	public test(name: string, fn: TestContext.Testable) {
-		const testId = this._tests.size + 1;
-		this._tests.add(async () => {
-			let ok = true;
-			try {
-				await fn({
-					name,
-					test: async (name, fn) => {
-						this.test(name, fn);
-					},
-				});
-			} catch (_) {
-				ok = false;
-			}
-			console.log(`${ok ? "ok" : "not ok"} ${testId} ${name}`);
-		});
-	}
-	public async run() {
-		console.log("TAP version 14");
-		for (const test of this._tests) {
-			await test();
-		}
+// This is an implementation of the TAP protocol version 14 in TypeScript.
+// Specification: https://testanything.org/tap-version-14-specification.html
+// Terminology follows the TAP nomenclature, and aims to stay dependency-free.
+
+// tiny assert implementation ッ
+function ッ<T = boolean>(cond: T, msg?: string) {
+	if (!cond) {
+		throw new Error(msg);
 	}
 }
+
+namespace formalize {
+	export const EmptyString = "";
+	export const Indentation = "  ";
+	export function description(text: string) {
+		return text.startsWith(" - ") ? text : ` - ${text}`;
+	}
+	export function escape(text: string) {
+		return text.replace(/\\/g, "\\\\").replace(/"/g, '\\"');
+	}
+}
+
+export enum Directive {
+	Skip,
+	Todo,
+}
+export enum Status {
+	Ok,
+	NotOk,
+}
+export interface TestPointInitializer {
+	readonly fn?: Fn;
+	readonly name?: string;
+	readonly scope?: TestPoint;
+	readonly directive?: Directive;
+}
+export interface TestPlanConfiguration {
+	readonly timeout?: number;
+	readonly signal?: AbortSignal;
+	readonly skip?: boolean | string;
+	readonly todo?: boolean | string;
+	readonly only?: boolean;
+	readonly concurrency?: number;
+}
+export interface TestRunConfiguration {
+	readonly timeout?: number;
+	readonly signal?: AbortSignal;
+}
+export interface TestPlanWithDirective {
+	(this: TestPoint): void;
+	skip(this: TestPoint): void;
+	todo(this: TestPoint): void;
+}
+
+// internal to the library, not exported
+const IT = Symbol("QuickTap/TestPoint/it");
+const TEST = Symbol("QuickTap/TestPoint/test");
+const DESCRIBE = Symbol("QuickTap/TestPoint/describe");
+
+export class TestPoint {
+	public readonly scope?: TestPoint;
+	public readonly subtests = new Set<TestPoint>();
+	public readonly name: string;
+	public readonly directive?: Directive;
+	public readonly fn: Fn;
+	public status?: Status;
+	public readonly reporter: (statement: string) => void;
+	public readonly indent = this.scope ? this.scope.indent + formalize.Indentation : formalize.EmptyString;
+	constructor(
+		{ name, fn = () => {}, directive = undefined, scope = undefined }: TestPointInitializer,
+		reporter?: (statement: string) => void,
+	) {
+		this.reporter = reporter || console.log;
+		this.scope = scope;
+		this.name = name || "<anonymous>";
+		this.directive = directive;
+		this.fn = fn;
+	}
+	public readonly isRoot = this.scope === undefined;
+	public readonly run = _run.bind(this);
+	public readonly it: TestPlanWithDirective = _it.bind(this);
+	public readonly test: TestPlanWithDirective = _it.bind(this);
+	public readonly describe: TestPlanWithDirective = _describe.bind(this);
+	async [DESCRIBE](name: string, options: TestPlanConfiguration, fn: DescribeFn) {}
+	async [TEST](name: string, options: TestPlanConfiguration, fn: TestFn) {}
+	async [IT](name: string, options: TestPlanConfiguration, fn: ItFn) {}
+	public [Symbol.toPrimitive]() {
+		return this.toString();
+	}
+	public toString() {
+		return JSON.stringify(this);
+	}
+	public toJSON() {
+		ッ(this.status, "Cannot serialize a test point that has not been ran yet.");
+		return {
+			name: this.name,
+			directive: this.directive,
+		};
+	}
+}
+async function _run(this: TestPoint) {
+	if (this.isRoot) {
+		this.reporter("TAP version 14");
+	}
+	this.reporter(`1..${this.subtests.size}`);
+	await Promise.all([...this.subtests].map((test) => test.run()));
+}
+async function _it(this: TestPoint, name: string, options: TestPlanConfiguration, fn: ItFn) {
+	await this[IT](name, options, fn);
+}
+_it.skip = async function (this: TestPoint, name: string, options: TestPlanConfiguration, fn: ItFn) {
+	await this[IT](name, { ...options, skip: true }, fn);
+};
+_it.todo = async function (this: TestPoint, name: string, options: TestPlanConfiguration, fn: ItFn) {
+	await this[IT](name, { ...options, todo: true }, fn);
+};
+async function _describe(this: TestPoint, name: string, options: TestPlanConfiguration, fn: DescribeFn) {
+	await this[DESCRIBE](name, options, fn);
+}
+_describe.skip = async function (this: TestPoint, name: string, options: TestPlanConfiguration, fn: DescribeFn) {
+	await this[DESCRIBE](name, { ...options, skip: true }, fn);
+};
+_describe.todo = async function (this: TestPoint, name: string, options: TestPlanConfiguration, fn: DescribeFn) {
+	await this[DESCRIBE](name, { ...options, todo: true }, fn);
+};
+async function _test(this: TestPoint, name: string, options: TestPlanConfiguration, fn: TestFn) {
+	await this[TEST](name, options, fn);
+}
+_test.skip = async function (this: TestPoint, name: string, options: TestPlanConfiguration, fn: TestFn) {
+	await this[TEST](name, { ...options, skip: true }, fn);
+};
+_test.todo = async function (this: TestPoint, name: string, options: TestPlanConfiguration, fn: TestFn) {
+	await this[TEST](name, { ...options, todo: true }, fn);
+};
+
+const root = new TestPoint({ name: "" });
+export const run = root.run;
+export const describe = root.describe;
+export const test = root.test;
+export const it = root.it;
+
+export type Fn = ItFn | TestFn | DescribeFn;
+export interface ItFn {
+	(done?: (error?: any) => void): void | Promise<void>;
+}
+export interface DescribeFn {
+	(context: SuiteContext): void | Promise<void>;
+}
+export interface TestFn {
+	(context: TestContext, done?: (error?: any) => void): void | Promise<void>;
+}
+
 export interface SuiteContext {
 	readonly name: string;
-	// // readonly signal: AbortSignal;
+	readonly signal: AbortSignal;
 }
 export interface TestContext {
 	readonly name: string;
-	// // readonly signal: AbortSignal;
-	// // beforeEach(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
-	// // afterEach(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
-	// // beforeAll(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
-	// // afterAll(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
-	// // before(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
-	// // after(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
-	// // diagnostic(message: string): void;
-	// // runOnly(shouldRunOnlyTests: boolean): void;
-	// // skip(message?: string): void;
-	// // todo(message?: string): void;
-	// // test(
-	// // 	name: string,
-	// // 	options: TestContext.TestOptions,
-	// // 	fn: (context: TestContext) => void | Promise<void>,
-	// // ): Promise<void>;
-	test(name: string, fn: (context: TestContext) => void | Promise<void>): Promise<void>;
+	readonly signal: AbortSignal;
+	beforeEach(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
+	afterEach(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
+	beforeAll(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
+	afterAll(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
+	before(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
+	after(fn: TestContext.HookFn, options?: TestContext.HookOptions): void;
+	diagnostic(message: string): void;
+	runOnly(shouldRunOnlyTests: boolean): void;
+	skip(message?: string): void;
+	todo(message?: string): void;
+	test(name: string, options: TestPlanConfiguration, fn: TestFn): Promise<void>;
 }
 export namespace TestContext {
-	export type Testable = (context: TestContext) => any;
-	export type Callable = (...input: any) => any;
-	export type HookFn = (context: TestContext) => any | Promise<any>;
+	export type HookFn = (context: TestContext) => any;
 	export interface HookOptions {
 		readonly timeout?: number;
 		readonly signal?: AbortSignal;
 	}
-	export interface TestOptions {
-		readonly timeout?: number;
-		readonly signal?: AbortSignal;
-		readonly skip?: boolean | string;
-		readonly todo?: boolean | string;
-		readonly only?: boolean;
-		readonly concurrency?: number;
-	}
 }
-
-const harness = new Harness();
-const test = harness.test.bind(harness);
-const describe = harness.describe.bind(harness);
-const it = harness.it.bind(harness);
-const run = harness.run.bind(harness);
-export { test, describe, it, run };
